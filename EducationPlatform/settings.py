08ff31781139fd33fa9a7e1409183ab72745117c
Kostyshyn-Max <mk0996417892@gmail.com>
2021-11-29 14:35:55 +0200

    Change allowed host property

--- EducationPlatform/settings.py
+++ EducationPlatform/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ed-platform-space.herokuapp.com', 'ed-platform.space']
+ALLOWED_HOSTS = ['ed-platform-space.herokuapp.com']
 
 
 # Application definition
