b849bf98d06b5b7246cd91fc44633e28bd73815c
Kostyshyn-Max <mk0996417892@gmail.com>
2021-12-05 19:41:02 +0200

    Change login and register actions

--- EducationPlatform/settings.py
+++ EducationPlatform/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ed-platform.space"]
+ALLOWED_HOSTS = ["ed-platform-space.herokuapp.com"]
 
 
 # Application definition
