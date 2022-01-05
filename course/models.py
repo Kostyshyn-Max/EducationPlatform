9de59e901a27a0c4cc6084fa1190e0d58d34dca2
Kostyshyn-Max <mk0996417892@gmail.com>
2022-01-05 12:33:13 +0200

    Change module model

--- course/models.py
+++ course/models.py
@@ -3,37 +3,49 @@ from django.db.models.deletion import CASCADE
 from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+
 class Module(models.Model):
     name = models.CharField(max_length=255)
+    id_for_roman = models.CharField(max_length=255, default="")
 
-class Lesson(models.Model): 
+
+class Lesson(models.Model):
     name = models.CharField(max_length=255)
     theory = models.TextField()
-    module = models.ForeignKey(Module, related_name="lessons", on_delete=models.CASCADE)
+    module = models.ForeignKey(
+        Module, related_name="lessons", on_delete=models.CASCADE)
+
 
 class LessonUser(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     lessons = models.ManyToManyField(Lesson)
+
 
 class Exercise(models.Model):
     name = models.CharField(max_length=255)
     condition = models.TextField()
     sample_input = models.TextField()
     sample_output = models.TextField()
-    lesson = models.ForeignKey(Lesson, related_name="exercises", on_delete=models.CASCADE)
+    lesson = models.ForeignKey(
+        Lesson, related_name="exercises", on_delete=models.CASCADE)
+
 
 class InputOutputData(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     input = models.TextField()
     output = models.TextField()
 
+
 class Attempt(models.Model):
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     code = models.TextField()
     pub_date = models.DateTimeField()
-    result = models.BooleanField() 
+    result = models.BooleanField()
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    
+
+
 class UserCustom(models.Model):
-    user = models.ForeignKey(get_user_model(), related_name="user_base", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        get_user_model(), related_name="user_base", on_delete=models.CASCADE)
     last_visited_page = models.CharField(max_length=255)
